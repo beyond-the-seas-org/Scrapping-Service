9d668eac61153ade3b3da04312a26703bb73a723
FAHIM <1805113@ugrad.cse.buet.ac.bd>
2023-09-08 21:12:55 +0600

    Location scrapping done from bestplace.net

---
+++ Location_scrapper/Send_locations.py
@@ -0,0 +1,14 @@
+#After running this py file all the locations in "location_details.json" will be inserted into the database of "Analytic Service"
+
+import json
+import requests
+locations =json.load(open("location_details_main.json", encoding="utf8"))
+
+
+try:
+    response = requests.post('http://localhost:5003/api/analytics/add_locations',json= locations)
+    print(response.json()['message'])
+
+
+except Exception as e:
+    print(e)
